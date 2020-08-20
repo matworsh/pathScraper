deca46aefe24decb7cd0969730214af547ff1bd7
Matt Worsham <matworsh@gmail.com>
2020-08-20 14:10:32 -0400

    changed to using playwright, changed which page to scrape from

--- server/helpers.js
+++ server/helpers.js
@@ -11,18 +11,7 @@ async function asyncForEach(array, callback) {
     }
 }
 
-/* function trimHTML(html, start, end){
-    trimedHTML = html[html.index(start)+len(start) : html.index(end)]
-    trimedHTML = trimedHTML.replace('<br/>','\n')
-    trimedHTML = trimedHTML.replace('<i>','')
-    trimedHTML = trimedHTML.replace('</i>','')
-    trimedHTML = trimedHTML.replace(u'\u2014','--')
-    trimedHTML = trimedHTML.replace(u'\u2019',"'")
-    return tmp
-}  */
-
 module.exports = {
     cheerioPage,
-    asyncForEach, 
-//    trimHTML
+    asyncForEach
 }
--- server/scrapers.js
+++ server/scrapers.js
@@ -1,19 +1,21 @@
-const puppeteer = require('puppeteer')
+const {chromium} = require('playwright')
 const helpers = require('./helpers');
 const classes = require('./classes')
-const urlBegin = 'https://2e.aonprd.com/'
-
+const urlBegin = 'https://pf2.d20pfsrd.com'
 
 
 async function scrapeAll(){
 
 }
 
-async function scrapeAncestries(url){
+/* async function scrapeAncestries(url){
   const ancestries = []
-  const browser = await puppeteer.launch();
+  const browser = await chromium.launch();
   const mainPage = await browser.newPage();
-  await mainPage.goto(url);
+  await mainPage.goto(url, {
+      waitUntil: 'load', 
+      timeout: 0
+  });
 
   const $ = await helpers.cheerioPage(mainPage);
 
@@ -32,11 +34,51 @@ async function scrapeAncestries(url){
 
   console.log(ancestries);
   return ancestries;
+} */
+
+async function scrapeAncestries(){
+    const ancestries = []
+    const browser = await chromium.launch();
+    const mainPage = await browser.newPage();
+    await mainPage.goto(urlBegin + '/ancestry', {
+        waitUntil: 'load', 
+        timeout: 0
+    });
+  
+    const $ = await helpers.cheerioPage(mainPage);
+  
+    const ancestryUrlList = [];
+    //get all the ancestry urls on initial page
+    $('#archive-data-table > tbody > tr').each((index, element) => {
+        if (index === 0) return true;
+        const tds = $(element).find('td');
+        const ancestry = tds[0];
+        const ancestryUrl = ancestry.firstChild.attribs['href'];
+        const publisher = $(tds[1]).text();
+        if(publisher !== 'Paizo'){
+            return;
+        }
+        ancestryUrlList.push(ancestryUrl);
+    })
+
+    //fill ancestry objects from ancestry pages
+    await helpers.asyncForEach (ancestryUrlList, async function(url, index){
+        let ancestryObj = await scrapeAncestry(url, browser, new classes.Ancestry());
+        ancestries.push(ancestryObj);
+    });
+  
+    browser.close();
+  
+    console.log(ancestries);
+    return ancestries;
 }
 
 async function scrapeAncestry(url, browser, ancestry){
     let page = await browser.newPage();
-    await page.goto(urlBegin + url);
+    await page.goto(urlBegin + url, {
+        waitUntil: 'load', 
+        timeout: 0
+    });
     let $ = await helpers.cheerioPage(page);
     //scrape name
     ancestry.Name = $('span#ctl00_MainContent_DetailedOutput>h1:nth-child(1)>a:nth-child(2)').text();
@@ -48,7 +90,11 @@ async function scrapeAncestry(url, browser, ancestry){
     });
     
     //scrape description
-    let general = "";
+    //let generalRange = await page.$$('xpath=//*[@id="ctl00_MainContent_DetailedOutput"] >> text="You Might..." >>');
+    //generalRange.filter(helpers.isText);
+    //*[@id="ctl00_MainContent_DetailedOutput"]/i/text()
+    //*[@id="ctl00_MainContent_DetailedOutput"]/text()[1]
+    let general = $('span[id="ctl00_MainContent_DetailedOutput"]').nextUntil('h2').filter(helpers.isText);
     general.each((index, desc) =>{
         ancestry.General.push($(desc).text())
     });
@@ -56,8 +102,8 @@ async function scrapeAncestry(url, browser, ancestry){
     return ancestry;
 }
 
-scrapeAncestries(urlBegin + 'Ancestries.aspx');
-
+//scrapeAncestries(urlBegin + 'Ancestries.aspx');
+scrapeAncestries();
 module.exports = {
     scrapeAll,
     scrapeAncestries,
