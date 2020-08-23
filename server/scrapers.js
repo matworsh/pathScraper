c7159bdf5a3e132cf5c6b2d248449b914f7e7fa0
Matt Worsham <matworsh@gmail.com>
2020-08-23 02:40:55 -0400

    found easier site to scrape from, using fetch instead of playwright

--- server/scrapers.js
+++ server/scrapers.js
@@ -1,8 +1,12 @@
-const {chromium} = require('playwright')
+const {chromium} = require('playwright');
 const helpers = require('./helpers');
-const classes = require('./classes')
-const urlBegin = 'https://pf2.d20pfsrd.com'
-
+const classes = require('./classes');
+const fetch = require('node-fetch');
+const zlib = require('zlib');
+const cheerio = require('cheerio');
+const request = require('request-promise');
+//const urlBegin = 'https://pf2.d20pfsrd.com'
+const urlBegin = 'https://pf2.easytool.es/tree/';
 
 async function scrapeAll(){
 
@@ -104,7 +108,8 @@ async function scrapeAncestry(url, source,  browser, ancestry){
     return ancestry;
 }
 
-async function scrapeCreatures(){
+/* async function scrapeCreatures(){
+    //<span class='menu'>Zuipnyrn</span><span class='level mr-0' data-toggle='tooltip' data-placement='right' title='rare'>3</span><input type='hidden' class='id' value='5534' /></button></div>
     const creatures = []
     const browser = await chromium.launch();
     const mainPage = await browser.newPage();
@@ -131,46 +136,172 @@ async function scrapeCreatures(){
     })
 
     //fill creatire objects from creature pages
-    await helpers.asyncForEach (ancestryUrlList, async function(url, index){
-        let ancestryObj = await scrapeCreature(url, browser, new classes.Creature());
-        ancestries.push(ancestryObj);
+    await helpers.asyncForEach (creatureUrlList, async function(url, index){
+        let creatureObj = await scrapeCreature(url, browser, new classes.Creature());
+        creatures.push(creatureObj);
     });
   
     browser.close();
   
-    console.log(ancestries);
-    return ancestries;
-}
+    console.log(anccreaturesestries);
+    return creatures;
+} */
 
-async function scrapeCreature(url, browser, creature){
-    let page = await browser.newPage();
-    await page.goto(url, {
-        waitUntil: 'load', 
-        timeout: 0
+async function scrapeCreatures(){
+    const creaturesFetch = await fetch("https://pf2.easytool.es/tree/loadBranches.php", {
+        "headers": {
+          "accept": "text/html, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+          "cookie": "PHPSESSID=8klrol4p94kvbvoekqmfiofh40; _mode=dark"
+        },
+        "referrer": "https://pf2.easytool.es/tree/index.php",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": "id_feature=64",
+        "method": "POST",
+        "mode": "cors"
+      });
+    
+    const cratureListHtml = await creaturesFetch.text();
+    const creatures = [];
+    const creatureUrlSettings = [];
+    const $ = await cheerio.load(cratureListHtml); // helpers.cheerioPage(mainPage);
+
+    //get all the creature urls on initial page
+    let allCreatureNames = $('span.menu').contents();
+    let allCreatureIds =  $('span.level ~ input[type="hidden"].id');
+    allCreatureIds.each((index, elem) => {
+        creatureUrlSettings.push(
+            {
+                id: elem.attribs['value'], 
+                name: allCreatureNames[index].data
+            });
+    })
+
+ /*    $('#archive-data-table > tbody > tr').each((index, element) => {
+        if (index === 0) return true;
+        const tds = $(element).find('td');
+        const creature = tds[0];
+        const creatureUrl = creature.firstChild.attribs['href'];
+        const publisher = $(tds[3]).text();
+        //const source = $(tds[4]).text();
+        if(publisher !== 'Paizo'){
+            return;
+        }
+        creatureUrlList.push(creatureUrl);
+    }) */
+
+    //fill creatire objects from creature pages
+    await helpers.asyncForEach (creatureUrlSettings, async function(obj, index){
+        let creatureObj = await scrapeCreature(obj, new classes.Creature());
+        creatures.push(creatureObj);
     });
-    let $ = await helpers.cheerioPage(page);
-    //scrape name
-    creature.Name = $('article.monster > h1').text();
+  
+    browser.close();
+  
+    console.log(creatures);
+    return creatures;
+}
+async function scrapeCreature(urlParams, creature){
+    const creatureHtml = await fetch(`https://pf2.easytool.es/tree/actionInfo3.php`, {
+        "headers": {
+          "accept": "text/html, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+          "cookie": "PHPSESSID=8klrol4p94kvbvoekqmfiofh40; _mode=dark"
+        },
+        "referrer": `https://pf2.easytool.es/tree/index.php?id=${urlParams.id}&name=${urlParams.name.toLowerCase()}`,
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": `id_feature=${urlParams.id}&advancement=0&type=0&optional=opt0`,
+        "method": "POST",
+        "mode": "cors"
+      });
+    
+    let $ = await cheerio.load(await creatureHtml.text());
 
+    //scrape name
+    creature.Name = $('header.pt-2 > h1.pt-2').text();
+
+    //scrape level
     creature.Level.Type = $('div.article-content > h4.monster > span.monster-type').text();
     creature.Level.Level = $('div.article-content > h4.monster > span.monster-level').text();
+
     //scrape traits
-    creature.Traits.Rarity = $('article.Monster > div.page-center > div.article-content > p.traits > span[class*="frequency"]');
-    creature.Traits.Alignment = $('article.Monster > div.page-center > div.article-content > p.traits > span.alignment');
-    creature.Traits.Size = $('article.Monster > div.page-center > div.article-content > p.traits > span.size');
-    let otherTraits = $('article.Monster > div.page-center > div.article-content > p.traits > span.trait');
+    creature.Traits.Rarity = $('article.monster > div.page-center > div.article-content > p.traits > span[class*="frequency"]').text();
+    creature.Traits.Alignment = $('article.monster > div.page-center > div.article-content > p.traits > span.alignment').text();
+    creature.Traits.Size = $('article.monster > div.page-center > div.article-content > p.traits > span.size').text();
+    let otherTraits = $('article.monster > div.page-center > div.article-content > p.traits > span.trait');
     otherTraits.each((index, trait) =>{
         creature.Traits.OtherTraits.push($(trait).text())
     });
 
-    creature.Source = ''
+    //scrape perception
+    let senses = $('article.monster > div.page-center > div.article-content > p > b:contains("Senses")').parent();
+    senses.find("*").addBack().contents().filter((i,s) => { return (s.type === "text" && $(s).text() !== "Senses")}).each((index, sense) =>{
+        if(index === 0){
+            creature.Perception.Modifier = $(sense).text();
+            return;
+        }
+        creature.Perception.SpecialSenses.push($(sense).text());
+    });
+    //scrape languages
+    let languages = $('article.monster > div.page-center > div.article-content > p > b:contains("Languages")').parent();
+    languages.find("*").addBack().contents().filter((i,s) => { return (s.type === "text" && $(s).text() !== "Languages")}).each((index, language) =>{
+        creature.Languages.push($(language).text());
+    });
+    //scrape skills
+    let skills = $('article.monster > div.page-center > div.article-content > p > b:contains("Skills")').parent();
+
+    let skillNames = skills.children("a");
+    skillNames.each((i,s) => {
+        creature.Skills.push({Name: $(s.firstChild).text(), Modifier: $(s.next).text()})
+    })
+
+    //scrape ability modifiers
+    //need to fix below this
+    let abilities = $('article.monster > div.page-center > div.article-content > p > b:contains("Str")');
+    abilities.contents().filter((i,s) => { return (s.type === "text" && !s.parentNode)}).each((index, ability) =>{
+       switch(index){
+        case 0:
+            creature.AbilityModifiers.Str = $(ability).text();
+            return;
+        case 1:
+            creature.AbilityModifiers.Dex = $(ability).text();
+            return;
+        case 2:
+            creature.AbilityModifiers.Con = $(ability).text();
+            return;
+        case 3:
+            creature.AbilityModifiers.Int = $(ability).text();
+            return;
+        case 4:
+            creature.AbilityModifiers.Wis = $(ability).text();
+            return;
+        case 5: 
+            creature.AbilityModifiers.Cha = $(ability).text();
+            return;
+        default:
+            return;
+       }
+    });
+    //scrape source
+    creature.Source = $('div.secion15 > div:nth-child(2) > a').text();
 
     //creature.lastScraped = new Date().toJson();
-    return ancestry;
+    return creature;
 }
 
 //scrapeAncestries(urlBegin + 'Ancestries.aspx');
-scrapeAncestries();
+//scrapeAncestries();
+scrapeCreatures();
 module.exports = {
     scrapeAll,
     scrapeAncestries,
